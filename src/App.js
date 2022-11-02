721cb16f58933324a7e86a34ee9201bbedcbfa77
James Lee <leewljames@gmail.com>
2022-11-02 21:41:41 +0800

    Chapter 9 - React Router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import GlobalStyle from "./components/GlobalStyle";
 
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 import Nav from './components/Nav';
+//Router
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
       <Nav />
-      <AboutUs/>
+      <Routes>
+        <Route exact path="/" element={<AboutUs/ >}></Route>
+        <Route path="/work" element={<OurWork/ >}></Route>
+        <Route path="/contact" element={<ContactUs/ >}></Route>
+      </Routes>
     </div>
   );
 }
