611003fddf9b11fc1cd730e02df8736051ef3110
James Lee <leewljames@gmail.com>
2022-11-07 20:47:27 +0800

    Chapter 13 - About Animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,8 +1,18 @@
+//Animations
+import {motion} from 'framer-motion';
+import {pageAnimation} from '../animation';
+
 const ContactUs = () => {
     return(
-        <div>
+        <motion.div
+            variants={pageAnimation} 
+            initial="hidden" 
+            animate="show"
+            exit="exit"
+            style={{background: "#fff"}}
+        >
             <h1>Our CONTACT</h1>
-        </div>
+        </motion.div>
     )
 }
 
